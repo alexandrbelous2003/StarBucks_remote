cd74edef5ba9881fcff9258f2b76c1b8e133271b
Alexandr Belous <aa9499196@gmail.com>
2022-11-08 16:06:07 +0500

    Initial Commit

---
+++ StarBucks/StarBucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarBucksApp.swift
+//  StarBucks
+//
+//  Created by Александр Белоус on 08.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct StarBucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
